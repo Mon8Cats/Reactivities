8c5c2b1ff1c46f242c859f8458062f3c5340aa64
StoneRoot <stoneroot@StoneRoots-MBP.lan>
2021-01-04 16:10:07 -0600

    Add style

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,9 +13,16 @@ class ActivityStore {
     @observable target = '';
 
     @computed get activitiesByDate(){
-        //return this.activities.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
-        return Array.from(this.activityRegistry.values())
-            .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+    }
+
+    groupActivitiesByDate(activities: IActivity[]) {
+        const sortedActivities = activities.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        return Object.entries(sortedActivities.reduce((activities, activity) => {
+            const date = activity.date.split('T')[0];
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as {[key: string]: IActivity[]}));
     }
 
     @action loadActivities = async () => {
@@ -66,14 +73,6 @@ class ActivityStore {
     getActivity = (id: string) => {
         return this.activityRegistry.get(id);
     }
-
-    /*
-    @action selectActivity = (id: string) => {
-        //this.selectedActivity = this.activities.find(a => a.id === id);
-        this.activity = this.activityRegistry.get(id);
-        //this.editMode = false;
-    };
-    */
 
 
     @action createActivity = async (activity: IActivity) => {
